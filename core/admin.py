2bcef8ca400aec77cee05bfbeca5defd8242b6f0
WenzileMira <simisodlamini002@gmail.com>
2026-01-06 19:43:24 +0200

    Fix video playback on GitHub Pages

--- core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Staff, Department, Donation
-# Register your models here.
-admin.site.register(Staff)
-admin.site.register(Department)
-admin.site.register(Donation)
